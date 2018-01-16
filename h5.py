41d7030493bd0efd89a66ecb3a32b580a1e16080
Leo <leo.guignard@gmail.com>
2018-01-16 16:53:21 -0500

    first commit

---
+++ h5.py
@@ -0,0 +1,13 @@
+from h5py import File
+from spatial_image import SpatialImage
+
+def read_h5(path):
+    """Read an hdf5 file
+
+    :Parameters:
+     - `filename` (str) - name of the file to read
+    """
+    data = File(path.replace('\\', ''), 'r')['Data']
+    im_out = np.zeros(data.shape, dtype=data.dtype)
+    data.read_direct(im_out)
+    return SpatialImage(im_out.transpose(2, 1, 0))
